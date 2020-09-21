9aa476d837cdfc02caae6de9763c2b4fc7aadf92
ericburgos <ericbu@amdocs.com>
2020-09-21 01:06:08 -0400

    Added logic to remove cards from table and hands properly. Also added points for player1. Also, changed isMatch method to checkTable and now returns points as int.

--- src/CuarentaGame.java
+++ src/CuarentaGame.java
@@ -4,6 +4,8 @@ import java.util.logging.Logger;
 import java.util.*;
 
 public class CuarentaGame {
+	public static int player1Points;
+	public static int player2Points;
 
     public static void main(String[] args) {
 
@@ -32,15 +34,17 @@ public class CuarentaGame {
         	if (tablePile.isEmpty()) {
         		boolean emptyTable = true;
         		p1Turn(player1Hand, tablePile, playerInput, emptyTable);
+        		System.out.println("Player 1 POINTS: " + player1Points );
         	}
         	else if (!tablePile.isEmpty()) {
         		boolean emptyTable = false;
         		p1Turn(player1Hand, tablePile, playerInput, emptyTable);
+        		System.out.println("Player 1 POINTS: " + player1Points );
         	}
             p2Turn(player2Hand, tablePile, playerInput);
         }
-
     }
+
 
     public static void p1Turn(Deck player1Hand, Deck tablePile, Scanner playerInput, Boolean isTurnWithEmptyTable) {
     	System.out.println("***Player 1's Turn***\n");
@@ -49,7 +53,6 @@ public class CuarentaGame {
         //System.out.println("Player2 Hand: " + player2Hand);
         //System.out.println("Player1 first card value : " + player1Hand.getCard(0).getNumVal());
         System.out.println("Which card # would you like to play? ");
-
         int rp = playerInput.nextInt();
         System.out.println("P1 of card playing: " + rp);
         System.out.println("Player 1 played: " + player1Hand.getCard(rp - 1));
@@ -72,10 +75,10 @@ public class CuarentaGame {
     }
         //player1Hand.removeCard(rp-1);
         if (isTurnWithEmptyTable) {
-        	isMatch(tablePile, player1Hand, rp, isTurnWithEmptyTable);
+        	player1Points += checkTable(tablePile, player1Hand, rp, isTurnWithEmptyTable);
         }
         else if (!isTurnWithEmptyTable) {
-        	isMatch(tablePile, player1Hand, rp, isTurnWithEmptyTable);
+        	player1Points += checkTable(tablePile, player1Hand, rp, isTurnWithEmptyTable);
         }
         //see if play is possible
     }
@@ -87,7 +90,7 @@ public class CuarentaGame {
         Random rand = new Random();
         int rp = rand.nextInt(player2Hand.deckSize());
         rp += 1;
-        System.out.println("P2 of card playing: " + rp);
+        System.out.println("P2 playing card # " + rp + " from hand");
         System.out.println("Player 2 played: " + player2Hand.getCard(rp-1));
         switch (rp) {
             case 1:
@@ -118,8 +121,9 @@ public class CuarentaGame {
         player2Hand.removeCard(rp-1);
     }
 
-    public static boolean isMatch(Deck tablePile, Deck playerHand, int rp, boolean emptyTable) {
-    	System.out.println("Im inside isMatch");
+    public static int checkTable(Deck tablePile, Deck playerHand, int rp, boolean emptyTable) {
+    	System.out.println("CHECKING TABLE FOR MATCHES . ." + "\n");
+    	int points = 0;
     	// Check to see if table is empty first before starting compare logic
     	if(!emptyTable) {
 
@@ -131,32 +135,45 @@ public class CuarentaGame {
 	        //caida check
 	        if (playerMatchCard == mostRecent) {
 
-	            System.out.println("CAIDA!- " + "Player card: "+ playerHand.getCard(rp-1) + playerMatchCard + " matches most recent card on table "
+	            System.out.println("CAIDA!- " + "Player card: "+ playerHand.getCard(rp-1) +
+	            " " + playerMatchCard + " matches most recent card on table "
 	            + tablePile.getCard(tablePile.deckSize() - 2) + " " + mostRecent);
+	            // Remove from hand
 	            playerHand.removeCard(rp-1);
+	            // Remove most recent from pile
+	            tablePile.removeCard(tablePile.deckSize() - 2);
+	            // Remove your card played on pile
+	            tablePile.removeCard(tablePile.deckSize() - 1);
 	            //play the matched cards, +2 points, caida
 
+	            points =  2;
 
-	            if(tablePile.isEmpty() == true){
-	                System.out.println("Limpia y Caida, +4");
+	            if(tablePile.isEmpty()){
+	                System.out.println("Caido Y Limpia!, +4");
+	                points += 2;
 	            }
 
-	            return true;
+	            return points;
 	        }
 
 	        //see if any card on the table matches the players chosen play card
+	        // Need to work on this - what if there's more than 1 card that matches?? - there shouldn't be any tbh
 	        else if(playerMatchCard != mostRecent) {
 	            for (int i = 0; i < tablePile.deckSize() - 1; i++) {
 	                if (playerMatchCard == tablePile.getCard(i).getNumVal()) {
-	                    System.out.println(playerHand.getCard(rp) + " matches " + tablePile.getCard(i));
+	                    System.out.println(playerHand.getCard(rp-1) + " matches " + tablePile.getCard(i));
+	                    playerHand.removeCard(rp-1);
+	                    tablePile.removeCard(tablePile.deckSize() - 1);
+	                    tablePile.removeCard(i);
 	                    //play the matched cards
-	                    return true;
+	                    return points;
 	                }
 	            }
 	        }
 
     	}
+    	System.out.println("No Matches :( ");
     	playerHand.removeCard(rp-1);
-    	return false;
+    	return points;
     }
 }
